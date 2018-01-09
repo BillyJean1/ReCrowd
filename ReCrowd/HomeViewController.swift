e1f1e66e50e9724762d6ec6432f612204bf67dfb
Zoë <blueberrymuffincakes@gmail.com>
2018-01-09 13:40:21 +0100

    finish reward screen

--- ReCrowd/HomeViewController.swift
+++ ReCrowd/HomeViewController.swift
@@ -12,9 +12,29 @@ class HomeViewController: UIViewController {
     
    public var user: User?
     
+    
     override func viewDidLoad() {
       // print(user?.id ?? "lol no user")
-        self.navigationController?.navigationBar.isHidden = true
+        //self.navigationController?.navigationBar.isHidden = true
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.navigationController?.navigationBar.topItem?.title = "Beloningen"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor:UIColor.white]
+        self.navigationController?.navigationBar.tintColor = UIColor.white
+        self.navigationController!.navigationBar.barTintColor = UIColor(red: 255/255.0, green: 167/255.0, blue: 20/255.0, alpha: 1.0)
+        
+        let coin = UIButton(type: .custom)
+        coin.setImage(#imageLiteral(resourceName: "CoinIcon"), for: .normal)
+        coin.tintColor = UIColor.white
+        coin.frame = CGRect(x: 2, y: 74, width: 140, height: 40)
+        coin.imageEdgeInsets = UIEdgeInsets(top: 6, left: 100, bottom: 6, right: 14)
+        coin.titleEdgeInsets = UIEdgeInsets(top: 0, left: -30, bottom: 0, right: 20)
+        // MARKER: hier zet je de coins van de user in de navbar
+        coin.setTitle("300", for: .normal)
+        
+        // add action?
+        let coinItem = UIBarButtonItem(customView: coin)
+        self.navigationController!.navigationBar.topItem?.setRightBarButtonItems([coinItem], animated: true)
     }
+
     
 }
