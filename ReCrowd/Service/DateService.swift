e3795ec6901c205a44f9f1cc2592de241b2c07a4
Kevin Broeren <root@kevinbroeren.nl>
2018-01-17 12:30:10 +0100

    App icons and fix for hours

--- ReCrowd/Service/DateService.swift
+++ ReCrowd/Service/DateService.swift
@@ -16,11 +16,11 @@ class DateService {
         let calendar = Calendar.current
         let hour = calendar.component(.hour, from: date)
         
-        if hour > 6 && hour < 12 {
+        if hour >= 6 && hour < 12 {
             return "Goedemorgen"
-        } else if hour > 12 && hour < 18 {
+        } else if hour >= 12 && hour < 18 {
             return "Goedemiddag"
-        } else if hour > 0 && hour < 6 {
+        } else if hour >= 0 && hour < 6 {
             return "Goedenacht"
         } else {
             return "Goedeavond"
